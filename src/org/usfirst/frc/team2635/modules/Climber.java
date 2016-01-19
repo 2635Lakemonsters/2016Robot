41afa02a34880a83f54ac4a1e5769ae20fad4295
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2016-01-18 18:47:45 -0800

    Made flywheel class, no modules made yet.

---
+++ src/org/usfirst/frc/team2635/modules/Climber.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team2635.modules;
+
+import com.lakemonsters2635.actuator.interfaces.BaseActuator;
+
+public class Climber {
+	BaseActuator<Double> climberActuator;
+	
+	
+}
---
+++ src/org/usfirst/frc/team2635/modules/Flywheel.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team2635.modules;
+
+import com.lakemonsters2635.actuator.interfaces.BaseActuator;
+import com.lakemonsters2635.composites.BaseLauncher;
+import com.lakemonsters2635.sensor.interfaces.ISensor;
+
+public class Flywheel {
+
+	BaseActuator<Double> leftWheel;
+	BaseActuator<Double> rightWheel;
+	
+	BaseActuator<Double> launcherFeed;
+	
+	BaseActuator<Double> frontLoader;
+	BaseActuator<Double> backLoader;
+	
+	ISensor<Boolean> canFireSensor;
+	public void fire(double magnitude, double feedMagnitude) {
+		leftWheel.actuate(magnitude);
+		rightWheel.actuate(magnitude);
+		if(canFireSensor.sense())
+		{
+			launcherFeed.actuate(feedMagnitude);
+		}
+		
+	}
+	public void loadBack(double magnitude)
+	{
+		backLoader.actuate(magnitude);
+	}
+	
+	public void loadFront(double magnitude)
+	{ 
+		frontLoader.actuate(magnitude);
+	}
+	
+}
